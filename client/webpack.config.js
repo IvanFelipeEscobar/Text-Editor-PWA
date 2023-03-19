657b86e235e67b724b679e71c0820e513121b0b8
Ivan Escobar <ivnescbr1989@gmail.com>
2023-03-19 15:00:01 -0400

    web pack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,13 +20,48 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template:`./index.html`,
-        title:`Text Editor`})
+        title:`Text Editor`
+      }),
+      new InjectManifest({
+        swSrc: `./src-sw.js`,
+        swDestL: `src-sw.js`
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: `Just Another Text Editor`,
+        short_name: `JATE`,
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: `./`,
+        publicPath: `./`,
+        icons: [
+          {
+            src: path.resolve(`src/images/logo.png`),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      })
     ],
-
     module: {
       rules: [
-        
-      ],
-    },
-  };
-};
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            }
+          }
+        }
+      ]
+    }
+  }
+}
